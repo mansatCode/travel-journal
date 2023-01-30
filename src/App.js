e03bd04d0dddfd5d0d355a9aaee0a2da17fc75cd
MansatCode <69149749+mansatCode@users.noreply.github.com>
2023-01-30 14:19:32 +0000

    Correct image displays

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Card from './components/Card';
 import Header from './components/Header';
+import LOCATIONS from './data/data';
 
 function App() {
+  const cards = LOCATIONS.map(item => {
+    return (
+      <Card
+        key={item.id}
+        {...item}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Header />
-      <Card />
+      <section className="cardsList">
+        {cards}
+      </section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import fuji from './assets/fuji.png'
 import marker from './assets/marker.png'
+import fuji from './assets/fuji.png'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
       <section className="card">
-        <img src={fuji} alt="Mt. Fuji"/>
+        <img src={require(`./assets/${props.imageUrl}`)} alt={props.title} className="card--image"/>
         <div className="card--textContainer">
           <div className="card--locationContainer">
             <img src={marker} alt="marker" className="card--marker"/>
-            <p className="card--location">Japan</p>
-            <a href="#" className="card--hyperlink">View on Google Maps</a>
+            <p className="card--location">{props.location}</p>
+            <a href={props.googleMapsUrl} rel="noreferrer"  target="_blank" className="card--hyperlink">View on Google Maps</a>
           </div>
-          <h1 className="card--header">Mount Fuji</h1>
-          <p className="card--dates">12 Jan, 2021 - 24 Jan, 2021</p>
-          <p className="card--description">Mount Fuji is the highest mountain in Japan at 3,776.24 m (12,389 ft). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+          <h1 className="card--header">{props.title}</h1>
+          <p className="card--dates">{props.startDate} - {props.endDate}</p>
+          <p className="card--description">{props.description}</p>
         </div>
       </section>
-      <hr className="card--divider"></hr>
     </div>
   )
 }
