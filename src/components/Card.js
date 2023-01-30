4ba2ff5922df1a0d31c66725d14e436e81fef79e
MansatCode <69149749+mansatCode@users.noreply.github.com>
2023-01-30 13:19:13 +0000

    Style one card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import fuji from './assets/fuji.png'
+import marker from './assets/marker.png'
 
 const Card = () => {
   return (
-    <h1>card</h1>
+    <div>
+      <section className="card">
+        <img src={fuji} alt="Mt. Fuji"/>
+        <div className="card--textContainer">
+          <div className="card--locationContainer">
+            <img src={marker} alt="marker" className="card--marker"/>
+            <p className="card--location">Japan</p>
+            <a href="#" className="card--hyperlink">View on Google Maps</a>
+          </div>
+          <h1 className="card--header">Mount Fuji</h1>
+          <p className="card--dates">12 Jan, 2021 - 24 Jan, 2021</p>
+          <p className="card--description">Mount Fuji is the highest mountain in Japan at 3,776.24 m (12,389 ft). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        </div>
+      </section>
+      <hr className="card--divider"></hr>
+    </div>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import logo from './assets/logo.png'
 
 const Header = () => {
   return (
-    <header>
-        <img src={require(logo)} alt="logo"/>
+    <header className="header">
+        <img src={logo} alt="logo" className="header--logo"/>
         <h3>my travel journal.</h3>
     </header>
   )
